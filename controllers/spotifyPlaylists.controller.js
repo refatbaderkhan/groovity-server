5ec8bf8b9dbc04899d7735b949932ef8022cb153
refatbaderkhan <refatbaderkhan@gmail.com>
2024-01-18 14:33:36 +0200

    added Spotify get category playlists controller and router

--- controllers/spotifyPlaylists.controller.js
+++ controllers/spotifyPlaylists.controller.js
@@ -15,7 +15,7 @@ const spotifyFeaturedPlaylists = async (req,res) => {
 
   try {
     const response = await axios(request);
-    res.json(response.data);
+    res.json(response.data); 
   }
 
   catch (error) {
@@ -23,4 +23,27 @@ const spotifyFeaturedPlaylists = async (req,res) => {
   } 
 }
 
-module.exports = spotifyFeaturedPlaylists;
+const spotifyCategoryPlaylists = async (req,res) => {
+
+  const token = localStorageAction('token');
+  const category_id = req.params.category_id;
+
+  const request = {
+    method: 'get',
+    url: `https://api.spotify.com/v1/browse/categories/${category_id}/playlists`,
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  };
+
+  try {
+    const response = await axios(request);
+    res.json(response.data); 
+  }
+
+  catch (error) {
+    console.log(error);
+  } 
+}
+
+module.exports = {spotifyFeaturedPlaylists, spotifyCategoryPlaylists}
--- routes/spotify.routes.js
+++ routes/spotify.routes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const spotifyNewReleases = require('../controllers/spotifyAlbums.contoller')
 const {spotifySeveralBrowseCategories, spotifySingleBrowseCategory} = require('../controllers/spotifyCategories.controller')
 const spotifyAvailableGenreSeeds = require('../controllers/spotifyGenres.controller')
-const spotifyFeaturedPlaylists = require('../controllers/spotifyPlaylists.controller')
+const {spotifyFeaturedPlaylists, spotifyCategoryPlaylists} = require('../controllers/spotifyPlaylists.controller')
 
 router.get('/new-releases', spotifyNewReleases);
 router.get('/several-browse-categories', spotifySeveralBrowseCategories)
 router.get('/single-browse-category/:category_id', spotifySingleBrowseCategory)
 router.get('/available-genre-seeds', spotifyAvailableGenreSeeds);
 router.get('/featured-playlists', spotifyFeaturedPlaylists);
+router.get('/category-playlists/:category_id', spotifyCategoryPlaylists);
 
 module.exports = router;
