d89842c9d93b1ab741a0ec8cf7bde9961b8c08be
refatbaderkhan <refatbaderkhan@gmail.com>
2024-01-18 13:29:17 +0200

    added Spotify get single browse category controller and router

--- controllers/spotifyCategories.controller.js
+++ controllers/spotifyCategories.controller.js
@@ -23,5 +23,27 @@ const spotifySeveralBrowseCategories = async (req,res) => {
   }
 }
 
+const spotifySingleBrowseCategory = async (req,res) => {
+  
+    const token = localStorageAction('token');
+  
+    const request = {
+      method: 'get',
+      url: 'https://api.spotify.com/v1/browse/categories/' + req.params.category_id,
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    };
+  
+    try {
+      const response = await axios(request);
+      res.json(response.data);
+    }
+  
+    catch (error) {
+      console.log(error);
+    }
+  }
 
-module.exports = spotifySeveralBrowseCategories;
+
+module.exports = {spotifySeveralBrowseCategories, spotifySingleBrowseCategory};
--- routes/spotify.routes.js
+++ routes/spotify.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const spotifyGenres = require('../controllers/spotifyGenres.controller')
 const spotifyReleases = require('../controllers/spotifyReleases.contoller')
-const spotifySeveralBrowseCategories = require('../controllers/spotifyCategories.controller')
+const {spotifySeveralBrowseCategories, spotifySingleBrowseCategory} = require('../controllers/spotifyCategories.controller')
 
 router.get('/genres', spotifyGenres);
 router.get('/releases', spotifyReleases);
-router.get('/several-browse-categories', spotifySeveralBrowseCategories);
+router.get('/several-browse-categories', spotifySeveralBrowseCategories)
+router.get('/single-browse-category/:category_id', spotifySingleBrowseCategory)
 
 
 module.exports = router;
