8e466100636d79a772a83dda1266258f9a75d682
refatbaderkhan <refatbaderkhan@gmail.com>
2024-01-20 15:49:42 +0200

    added Spotify get artist related artists controller and router

--- controllers/spotifyArtists.controller.js
+++ controllers/spotifyArtists.controller.js
@@ -97,8 +97,34 @@ const spotifyGetArtistTopTracks = async (req,res) => {
   }
 }
 
+
+const spotifyGetArtistRelatedArtists = async (req,res) => {
+
+  const token = localStorageAction('token');
+  const artist_id = req.params.artist_id;
+
+  const request = { 
+    method: 'get',
+    url: `https://api.spotify.com/v1/artists/${artist_id}/related-artists`,
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  };
+
+  try {
+    const response = await axios(request);
+    res.json(response.data);
+  }
+
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
 module.exports = {spotifyGetArtist, 
                   spotifyGetSeveralArtists,
                   spotifyGetArtistAlbums,
-                  spotifyGetArtistTopTracks
+                  spotifyGetArtistTopTracks,
+                  spotifyGetArtistRelatedArtists
                 };
--- routes/spotify.routes.js
+++ routes/spotify.routes.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const {spotifyGetAlbum, spotifyGetSeveralAlbums, spotifyGetAlbumTracks, spotifyNewReleases} = require('../controllers/spotifyAlbums.contoller')
-const {spotifyGetArtist, spotifyGetSeveralArtists, spotifyGetArtistAlbums, spotifyGetArtistTopTracks} = require('../controllers/spotifyArtists.controller')
-const {spotifySeveralBrowseCategories, spotifySingleBrowseCategory} = require('../controllers/spotifyCategories.controller')
+
+const {spotifyGetAlbum,
+       spotifyGetSeveralAlbums, 
+       spotifyGetAlbumTracks, 
+       spotifyNewReleases
+      } = require('../controllers/spotifyAlbums.contoller')
+
+const {spotifyGetArtist,
+       spotifyGetSeveralArtists, 
+       spotifyGetArtistAlbums, 
+       spotifyGetArtistTopTracks,
+       spotifyGetArtistRelatedArtists
+      } = require('../controllers/spotifyArtists.controller')
+      
+const {spotifySeveralBrowseCategories, 
+       spotifySingleBrowseCategory
+      } = require('../controllers/spotifyCategories.controller')
+
 const spotifyAvailableGenreSeeds = require('../controllers/spotifyGenres.controller')
-const {spotifyGetPlaylist, spotifyFeaturedPlaylists, spotifyCategoryPlaylists} = require('../controllers/spotifyPlaylists.controller')
+
+const {spotifyGetPlaylist, 
+       spotifyFeaturedPlaylists, 
+       spotifyCategoryPlaylists
+      } = require('../controllers/spotifyPlaylists.controller')
+
+
 
 router.get('/get-album/:album_id', spotifyGetAlbum);
 router.get('/get-several-albums/:album_ids', spotifyGetSeveralAlbums);
@@ -15,6 +36,7 @@ router.get('/get-artist/:artist_id', spotifyGetArtist);
 router.get('/get-several-artists/:artist_ids', spotifyGetSeveralArtists);
 router.get('/get-artist-albums/:artist_id', spotifyGetArtistAlbums);
 router.get('/get-artist-top-tracks/:artist_id', spotifyGetArtistTopTracks);
+router.get('/get-artist-related-artists/:artist_id', spotifyGetArtistRelatedArtists);
 
 router.get('/several-browse-categories', spotifySeveralBrowseCategories)
 router.get('/single-browse-category/:category_id', spotifySingleBrowseCategory)
@@ -24,5 +46,6 @@ router.get('/available-genre-seeds', spotifyAvailableGenreSeeds);
 router.get('/get-playlist/:playlist_id', spotifyGetPlaylist);
 router.get('/featured-playlists', spotifyFeaturedPlaylists);
 router.get('/category-playlists/:category_id', spotifyCategoryPlaylists);
+
 
 module.exports = router;
